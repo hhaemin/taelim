425a7e58dae4faf1f0f235bea935e70e91c7428a
hhaemin <ahrgoals@naver.com>
2024-12-30 11:22:03 +0900

    Footer 디자인 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,19 @@ const Footer = () => {
   return (
     <FooterContainer>
       <Content>
-        <Logo>TAELIM</Logo>
-        <Location>
+        <RightBox>
+          <Logo>TAELIM</Logo>
+          <Info>
+            <span>대표자: 이성한</span>
+            <span>tel: 031)764-7347</span>
+            <span>FAX: 031)763-2539</span>
+          </Info>
+        </RightBox>
+        <Info>
           <span>경기도 광주시 도척면 다람로 36번길(궁평리 668-1)65-71</span>
+          {/* <span>우편번호 : 12810</span> */}
           <span>ⓒ 2017 TAELIM Co.,Ltd</span>
-        </Location>
+        </Info>
       </Content>
     </FooterContainer>
   );
@@ -18,7 +26,7 @@ const Footer = () => {
 const FooterContainer = styled.div`
   position: absolute;
   bottom: 0;
-  height: 150px;
+  height: 130px;
   background: #ddd;
   width: 100vw;
   display: flex;
@@ -33,16 +41,21 @@ const Content = styled.div`
   margin-right: 250px;
 `;
 
+const RightBox = styled.div`
+  display: flex;
+  gap: 50px;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const Logo = styled.span`
   font-size: 40px;
   font-weight: 800;
   border-top: 10px solid red;
   border-bottom: 10px solid red;
-`;
-
-const Location = styled.div`
-  display: flex;
-  flex-direction: column;
 `;
 
 export default Footer;
